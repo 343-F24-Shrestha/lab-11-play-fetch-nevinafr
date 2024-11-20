48ef1212a5fc07d0a0e6e0adf6cf01a8a891ba96
Nevina Frese <fresenm@dukes.jmu.edu>
2024-11-20 13:33:00 -0500

    part 1 functionality

--- js/part1.js
+++ js/part1.js
@@ -3,8 +3,15 @@ const output = document.getElementById("output");
 document.getElementById("get-btn").addEventListener("click", async () => {
     // This function should send a GET request to the echo endpoint and output the result
     // The two input fields should be included in the request URL as **query parameters**
+    // name and age
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    let params = new URLSearchParams( {name, age} );
+    let resp = await fetch("https://echo.zuplo.io/api?" + params.toString()); // add '?' to print query
+    console.log(resp);
 
-    // TODO
+    let response = await resp.json();
+    output.textContent = JSON.stringify(response, null, 2); //not .innerHTML
 });
 
 document.getElementById("post-json-btn").addEventListener("click", async () => {
